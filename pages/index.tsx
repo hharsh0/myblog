1fef1042de22baec090a253c5a47bcc73c935387
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2023-02-01 17:29:58 +0530

    landing page change

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home() {
               <span className="text-[#DE1D8D] dark:text-[#DE1D8D]">Harsh</span>
             </h1>
             <h2 className="prose pt-5 text-lg text-gray-600 dark:text-gray-300">
-              {`Welcome to My Blog. I am a Data Engineer who is passionate about Data Science and Automation. In my free time, I like developing `}
+              {`Welcome to My Blog. I am a Part time front-end engineer who is passionate about Data Science and Automation. In my free time, I like developing `}
               side projects and learning new technologies.
             </h2>
             <p className="pt-5 text-lg relative leading-7 text-slate-600 dark:text-slate-300 sm:block md:hidden lg:hidden">
