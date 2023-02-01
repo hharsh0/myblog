578ce9a3a17976f458bea63394c32725e1f9aff9
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2023-02-01 17:57:21 +0530

    update dropmenu

--- components/DropMenu.tsx
+++ components/DropMenu.tsx
@@ -34,7 +34,7 @@ function DropMenu() {
       setIsOpen(!isOpen);
     };
 
-    const [ThemeSound] = useSound("/static/sounds/page-change.mp3");
+  const [ThemeSound] = useSound("/static/sounds/page-change.mp3");
 
   return (
     <Menu as="div" className="relative z-10 inline-block text-left ">
@@ -57,6 +57,162 @@ function DropMenu() {
           </motion.button>
         </Menu.Button>
       </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+        afterEnter={() => {
+          toggleIcon();
+          ThemeSound();
+        }}
+        afterLeave={() => {
+          toggleIcon();
+          ThemeSound();
+        }}
+      >
+        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-zinc-300 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:divide-zinc-700 dark:bg-zinc-800 ">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <Link href="/">
+                  <div
+                    className={classNames(
+                      active
+                        ? "bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300"
+                        : "bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    <div className="flex flex-row">
+                      <HomeIcon className="mr-4 mt-0.5" /> Home
+                    </div>
+                  </div>
+                </Link>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <Link href="/blog">
+                  <div
+                    className={classNames(
+                      active
+                        ? "bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300"
+                        : "bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    <div className="flex flex-row">
+                      <Pencil1Icon className="mr-4 mt-0.5" /> Blog
+                    </div>
+                  </div>
+                </Link>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <Link href="/projects">
+                  <div
+                    className={classNames(
+                      active
+                        ? "bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300"
+                        : "bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    <div className="flex flex-row">
+                      <ArchiveIcon className="mr-4 mt-0.5" /> Projects
+                    </div>
+                  </div>
+                </Link>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <Link href="/about">
+                  <div
+                    className={classNames(
+                      active
+                        ? "bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300"
+                        : "bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    <div className="flex flex-row">
+                      <PersonIcon className="mr-4 mt-0.5" /> About
+                    </div>
+                  </div>
+                </Link>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <>
+                  <Link href="">
+                    <div
+                      className={classNames(
+                        active
+                          ? "cursor-pointer bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300"
+                          : "bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700",
+                        "block cursor-pointer px-4 py-2 text-sm"
+                      )}
+                    >
+                      <div className="flex flex-row">
+                        <>
+                          <EnterIcon />
+                          <div className="ml-4">Sign In</div>
+                        </>
+                      </div>
+                    </div>
+                  </Link>
+                </>
+              )}
+            </Menu.Item>
+            <hr className="border-gray-700" />
+            <div className="py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <Link href="/contact">
+                    <div
+                      className={classNames(
+                        active
+                          ? "bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300"
+                          : "bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700",
+                        "block px-4 py-2 text-sm"
+                      )}
+                    >
+                      <div className="flex flex-row">
+                        <Link2Icon className="mr-4 mt-0.5" /> Contact
+                      </div>
+                    </div>
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link href="/guestbook">
+                    <div
+                      className={classNames(
+                        active
+                          ? "bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300"
+                          : "bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700",
+                        "block px-4 py-2 text-sm"
+                      )}
+                    >
+                      <div className="flex flex-row">
+                        <ChatBubbleIcon className="mr-4 mt-0.5" /> Guestbook
+                      </div>
+                    </div>
+                  </Link>
+                )}
+              </Menu.Item>
+            </div>
+          </div>
+        </Menu.Items>
+      </Transition>
     </Menu>
   );
 }
